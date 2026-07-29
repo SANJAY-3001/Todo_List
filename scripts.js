c7d4423e63cdd7b21de958677a3913bf84db96fb
SANJAY-3001 <sanjayp7639@gmail.com>
2026-07-29 20:35:07 +0530

    added functionality to every buttons

--- scripts.js
+++ scripts.js
@@ -5,13 +5,21 @@ let allTasks = [];
 const inputEl = document.getElementById("input-el");
 const addBtn = document.getElementById("add-btn");
 const ulEl = document.getElementById("ul-el");
+const allBtn = document.getElementById("all-btn");
+const activeBtn = document.getElementById("active-btn");
+const doneBtn = document.getElementById("done-btn");
+const clearBtn = document.getElementById("clear-btn");
+
+
+const buttons = [allBtn , activeBtn , doneBtn];
+
 
 myTasksFromLocalStorage =  JSON.parse(localStorage.getItem("myTasks"));
 
 if(myTasksFromLocalStorage)
 {
     allTasks = myTasksFromLocalStorage;
-    renderTasks(tasks);
+    renderTasks(allTasks , "All");
 }
 
 
@@ -23,8 +31,7 @@ addBtn.addEventListener("click" , function()
 
     inputEl.value = "";
 
-    const task = 
-    {
+    const task = {
         id : Date.now(),
         text : val,
         completed : false
@@ -35,20 +42,63 @@ addBtn.addEventListener("click" , function()
 
     localStorage.setItem("myTasks" , JSON.stringify(allTasks));
 
-    renderTasks(allTasks);
+    renderTasks(allTasks , "ALL");
+});
+
+for(let i=0;i<buttons.length;i++)
+{
+    buttons[i].addEventListener("click" , function()
+    {
+        for(let j=0;j<buttons.length;j++)
+        {
+            buttons[j].classList.remove("active");
+        }
+
+        buttons[i].classList.add("active");
+
+        renderTasks(allTasks , buttons[i].textContent);
+        // console.log(buttons[i].textContent);
+    });
+}
+
+clearBtn.addEventListener("click",function()
+{
+    allTasks = clearTasks(allTasks);
+    localStorage.setItem("myTasks" , JSON.stringify(allTasks));
 });
 
 
-function renderTasks(tasks)
+function renderTasks(tasks , status)
 {
     let listOfTask = "";
 
     for(let i=0;i<tasks.length;i++)
     {
-        listOfTask += `<li class="task"><input class="check-input" type="checkbox" id = "">
-        <span> ${tasks[i].text}</span>
-                </li>`
+        const currStatus = tasks[i].completed ? "Done" : "Active";
+
+        if(status == "All" || status == currStatus)
+        {
+            listOfTask += `<li class="task"><input class="check-input" type="checkbox" id = "">
+            <span> ${tasks[i].text}</span>
+                    </li>`
+        }
     }
 
     ulEl.innerHTML = listOfTask;
+}
+
+
+function clearTasks(tasks)
+{
+    let listOfActiveTasks = [];
+
+    for(let i=0;i<tasks.length;i++)
+    {
+        if(!tasks[i].completed)
+        {
+            listOfActiveTasks.push(tasks[i]);
+        }
+    }
+
+    return listOfActiveTasks;
 }
